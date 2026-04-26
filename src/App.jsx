19d6353f8e8c5e1eb1362162c9b514c6d5e13a20
gookcompsci <gookcompsci@hotmail.com>
2026-04-26 21:08:52 +0700

    ai edit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div className="fullscreen-container">
+            <img src="/convas_lp.svg" alt="MST Landing Page" />
+        </div>
+    )
+}
